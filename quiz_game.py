acbf4ffe951ae96702e2b3c998b73c740a405f9c
David Lopez <50810168+dlopez079@users.noreply.github.com>
2022-02-16 16:23:17 -0500

    Add project files.

---
+++ quiz_game.py
@@ -0,0 +1,9 @@
+# Welcome the player to the game.
+print("Welcome to my computer quiz!")
+
+# As the user if he or she would like to play.
+playing = input("Do you want to play? ")
+
+# Create a condition based on the answer that the user entered.
+if playing != "yes":
+    quit()
