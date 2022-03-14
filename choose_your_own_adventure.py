927c8e862b640164cc4b195058881f10711b1b4e
David Lopez <50810168+dlopez079@users.noreply.github.com>
2022-03-14 18:25:51 -0400

    Spelling correction.

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,7 +1,7 @@
 name = input('Type Your Name: ')
 print("Welcome,", name, "to this adventure!")
 
-answer = input('Your are on a dirt road, it has come to and end and you can go left or right.  Which way would youl like to go? ').lower
+answer = input('Your are on a dirt road, it has come to and end and you can go left or right.  Which way would you like to go? ').lower
 
 if answer == "left":
     answer = input("You come to a river, you can walk around it or swim across?  Type walk to walk around and swim to swim accross: ")
